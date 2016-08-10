cf8b07b75faf6756b10ad11b0967f523c8b0fafc
IvanSostarko <ivan.sostarko@hotmail.com>
2016-08-10 23:44:21 +0200

    Dashboard icons added

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -24,12 +24,12 @@
 			<tbody>
 
 
-			<tr><td><strong>Total Admins</strong></td><td>{{ $admins }}</td></tr>
-			<tr><td><strong>Total Users</strong></td><td>{{ $users }}</td></tr>
-			<tr><td><strong>Banned Users</strong></td><td>{{ $banned }}</td></tr>
-			<tr><td><strong>Unverified Users</strong></td><td>{{ $unverified }}</td></tr>
-			<tr><td><strong>Total Categories</strong></td><td>{{ $categories }}</td></tr>
-			<tr><td><strong>Total Items</strong></td><td>{{ $items }}</td></tr>
+			<tr><td><strong><i class="fa fa-user-secret" aria-hidden="true"></i> Total Admins</strong></td><td>{{ $admins }}</td></tr>
+			<tr><td><strong><i class="fa fa-users" aria-hidden="true"></i> Total Users</strong></td><td>{{ $users }}</td></tr>
+			<tr><td><strong><i class="fa fa-ban" aria-hidden="true"></i> Banned Users</strong></td><td>{{ $banned }}</td></tr>
+			<tr><td><strong><i class="fa fa-user-times" aria-hidden="true"></i> Unverified Users</strong></td><td>{{ $unverified }}</td></tr>
+			<tr><td><strong><i class="fa fa-file-text" aria-hidden="true"></i> Total Categories</strong></td><td>{{ $categories }}</td></tr>
+			<tr><td><strong><i class="fa fa-cutlery" aria-hidden="true"></i> Total Items</strong></td><td>{{ $items }}</td></tr>
 
 
 
@@ -50,8 +50,8 @@
 			<tbody>
 
 
-			<tr><td><strong>Online Guests</strong></td><td>{{ $get_online_guests }}</td></tr>
-			<tr><td><strong>Online Users</strong></td><td>{{ $get_online_users }}</td></tr>
+			<tr><td><strong><i class="fa fa-user-plus" aria-hidden="true"></i> Online Guests</strong></td><td>{{ $get_online_guests }}</td></tr>
+			<tr><td><strong><i class="fa fa-user" aria-hidden="true"></i> Online Users</strong></td><td>{{ $get_online_users }}</td></tr>
 			</tbody>
 
 		</table>
